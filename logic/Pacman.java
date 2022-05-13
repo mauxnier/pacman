36154d62d5a182bed76e4a6468a34282f3d662f1
Abdella <abdella@x-rator.com>
2022-05-13 12:10:50 +0200

    Implements move function

--- logic/Pacman.java
+++ logic/Pacman.java
@@ -5,10 +5,10 @@ import adding.Tuple2;
 
 public class Pacman implements IPacman {
 
-    Game game;
-    data.Pacman dataPacman;
-    int nbLives;
-    boolean isSuperpowered;
+    private Game game;
+    private static data.Pacman dataPacman;
+    private int nbLives;
+    private boolean isSuperpowered;
 
     /**
      * Constructeur de la classe.
@@ -21,8 +21,35 @@ public class Pacman implements IPacman {
     }
 
     @Override
-    public void move(){
+    public void move(String direction){
+        boolean isCorrectDirection = true;
+        Tuple2<Integer, Integer> newPos;
+        int actualPosX = dataPacman.getPos().getFirst();
+        int actualPosY = dataPacman.getPos().getSecond();
+        switch(direction){
+            case "UP": 
+                newPos = new Tuple2<Integer,Integer>(actualPosX+1, actualPosY);
+                break;
+        
+            case "DOWN":
+                newPos = new Tuple2<Integer,Integer>(actualPosX-1, actualPosY);
+                break;
+        
+            case "RIGHT":
+                newPos = new Tuple2<Integer,Integer>(actualPosX, actualPosY+1);
+                break;
 
+            case "LEFT":
+                newPos = new Tuple2<Integer,Integer>(actualPosX, actualPosY-1);
+                break;
+            default:
+                System.out.println("Direction incorrecte");
+                isCorrectDirection = false;
+                break;
+        }
+        if (!Grid.dataGrid.isPosAWall(newPos) && isCorrectDirection){
+            dataPacman.setDirection(direction);
+        }
     }
 
     @Override
@@ -69,5 +96,13 @@ public class Pacman implements IPacman {
         boolean isOnFruit = false;
         return isOnFruit;
 
+    }
+
+    public boolean getIsSuperpowered(){
+        return isSuperpowered;
+    }
+
+    public int getNbLives(){
+        return nbLives;
     }
 }
